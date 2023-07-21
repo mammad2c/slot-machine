0d5802fe39f4f30298f764dfa26c7af46d2c7933
Mohammad Toosi <m_dd@rogers.com>
2023-07-21 11:48:16 +0330

    chore: check game store itself in the test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,11 +1,14 @@
 import { renderComponent } from "@/tests/renderComponent";
 import App from "./App.vue";
+import { useGameStore } from "@/stores";
 
 describe(App.name, () => {
   it("should start the game when we click on the start game button", async () => {
     const { queryByText, user } = renderComponent(<App />);
+    const game = useGameStore();
     const startGameBtn = queryByText(/start the game/i);
     await user.click(startGameBtn);
     expect(queryByText(/welcome to slot machine/i)).not.toBeInTheDocument();
+    expect(game.status).toBe("playing");
   });
 });
