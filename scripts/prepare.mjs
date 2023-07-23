d3d7481ffa4bbf968e0a0e5980d99fa70d200fd4
Mohammad Toosi <m_dd@rogers.com>
2023-07-23 04:28:40 +0330

    chore: disable husky in CI environment

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -1,11 +1,15 @@
 import os from "node:os";
 import { exec } from "node:child_process";
+import ci from "ci-info";
 
-exec("husky install", (_, stdout) => {
-  console.log(stdout);
-});
+if (!ci.isCI) {
+  exec("husky install", (_, stdout) => {
+    console.log(stdout);
+  });
+}
 
-if (os.type() !== "Windows") {
+if (os.type() !== "Windows" && !ci.isCI) {
+  console.log("Enabled husky on non Windows systems");
   exec("chmod ug+x .husky/*", (_, stdout) => {
     console.log(stdout);
   });
